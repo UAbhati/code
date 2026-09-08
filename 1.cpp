6054fda7c96bffe00f4a4890dfb2632066929cb2
Ubaid <ubaidbhati2015@gmail.com>
2026-09-08 13:12:16 +0530

    cses Weird Algorithm

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define ll long long int
+
+int main() {
+    ll n;
+    std::cin >> n;
+    std::cout << n << " ";
+    while(n != 1) {
+        if(n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        std::cout << n << " ";
+    }
+    return 0;
+}
