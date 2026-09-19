75aa9430f545e66a57486551113e8cd374903424
Ubaid <ubaidbhati2015@gmail.com>
2026-09-19 16:11:44 +0530

    increasing array

---
+++ cses/increseArray.py
@@ -0,0 +1,8 @@
+n = int(input())
+nums = list(map(int,input().split()))
+count = 0
+for i in range(n-1):
+    if nums[i] > nums[i+1]:
+        count += nums[i] - nums[i+1]
+        nums[i+1] = nums[i]
+print(count)
