24f4c788c0c5a349830851520e14a79765fd1ab3
Ubaid <ubaidbhati2015@gmail.com>
2026-09-21 22:22:25 +0530

    group anagram

---
+++ leetcode/groupAnagram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        ans = defaultdict(list)
+        for s in strs:
+            key = ''.join(sorted(s))
+            ans[key].append(s)
+        return list(ans.values())
